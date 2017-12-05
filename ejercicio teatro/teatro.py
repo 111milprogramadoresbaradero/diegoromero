419f4b8b2701aeb831d477a5731eb973258f1961
Andres Garagiola <garagiol@ar.ibm.com>
2017-12-05 19:22:06 -0300

    Agrega validacion de butaca vendida.

--- ejercicio teatro/teatro.py
+++ ejercicio teatro/teatro.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 
 def leerPlanta():
-    planta = pd.DataFrame.from_csv("C:/Users/Diego/Documents/111mil/diegoromero/ejercicio teatro/planta.csv", sep=",", index_col="fila")
+    planta = pd.DataFrame.from_csv("planta.csv", sep=",", index_col="fila")
     return planta
 
 def mostrarPlanta():
@@ -26,27 +26,26 @@ def grabarVenta(numeroFila,columna):
     fila = fila.copy()
     fila[columna]=1
     planta.ix[numeroFila]=fila    
-    planta.to_csv("C:/Users/Diego/Documents/111mil/diegoromero/ejercicio teatro/planta.csv", sep=",")
+    planta.to_csv("planta.csv", sep=",")
 
 def imprimirEntrada(numeroFila,columna):
     print("Entrada para la Fila ",numeroFila," en la posicion ",columna)
     # implementar impresion en papell
 
 def estadisticas():
-    contadorAsientos = 0
-    contadorAsientosUsados = 0
-    planta = leerPlanta()
-    for index, row in planta.iterrows():
-        for asiento in row:
-            contadorAsientos = contadorAsientos + 1
-            if (asiento==1):
-                contadorAsientosUsados = contadorAsientosUsados + 1
-
+    planta = leerPlanta() # Planta es un objeto del tipo DataFrame
+    contadorAsientos = planta.count().sum()
+    contadorAsientosUsados = planta.sum().sum()
     print("total asientos: ",contadorAsientos)
     print("total usados: ",contadorAsientosUsados)
     print("porcentaje ocup.: ",(contadorAsientosUsados/contadorAsientos)*100)
 
-
+def estaVendidaButaca(fila, columna):
+    """Esta funcion retorna True si la butaca en la fila y columna pasadas por 
+    parametro esta vendida, en otro caso retorna False."""
+    planta = leerPlanta()
+    fila = planta.ix[fila]
+    return fila[columna] == 1
 
 def taquilla():
     continua = 'si'
@@ -54,10 +53,12 @@ def taquilla():
         mostrarPlanta()
         numeroFila = int(input("Ingrese Fila"))
         columna = input("Ingrese Columna")
-        grabarVenta(numeroFila, columna)
-        imprimirEntrada(numeroFila, columna)
+        if estaVendidaButaca(numeroFila, columna):
+            print("La butaca Fila=",numeroFila," Columna=",columna," fue vendida previamente.")
+        else:
+            grabarVenta(numeroFila, columna)
+            imprimirEntrada(numeroFila, columna)
         continua = input("continua? si/no")
     estadisticas()
-        
-        
+              
 taquilla()
