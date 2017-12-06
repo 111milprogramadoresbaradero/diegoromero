3be98f3b449c7b35c7d145891d2964ca7e28755c
Andres Garagiola <garagiol@ar.ibm.com>
2017-12-05 21:50:06 -0300

    Agrega validacion de columna, fila validas y estadisticas por fila y columna.

--- ejercicio teatro/teatro.py
+++ ejercicio teatro/teatro.py
@@ -34,6 +34,15 @@ def imprimirEntrada(numeroFila,columna):
 
 def estadisticas():
     planta = leerPlanta() # Planta es un objeto del tipo DataFrame
+    
+    print("Ocupacion por filas:")
+    ocu_filas = (planta.sum(axis=1) / planta.count(axis=1)) * 100 
+    print(ocu_filas)
+    
+    print("Ocupacion por columnas:")
+    ocu_columnas = (planta.sum(axis=0) / planta.count(axis=0)) * 100 
+    print(ocu_columnas)
+    
     contadorAsientos = planta.count().sum()
     contadorAsientosUsados = planta.sum().sum()
     print("total asientos: ",contadorAsientos)
@@ -47,17 +56,41 @@ def estaVendidaButaca(fila, columna):
     fila = planta.ix[fila]
     return fila[columna] == 1
 
+def esValidaFila(fila):
+    """Retorna True si la fila pasada como parametro, es una fila valida."""
+    planta = leerPlanta()
+    return fila in planta.index.values
+
+def esValidaColumna(columna):
+    """Retorna True si la columna pasada como parametro, es una columna valida."""
+    planta = leerPlanta()
+    return columna in planta
+
+def venderButaca(fila, columna):
+    """Vende la butaca correspondiente a la fila y la columna. Valida que no haya sido
+    vendida previamente. En caso de que la butaca este vendida levanta una excepcion"""
+    if estaVendidaButaca(fila, columna):
+        raise Exception("La butaca esta vendida")
+    else:
+        grabarVenta(fila, columna)
+        imprimirEntrada(fila, columna)
+
 def taquilla():
     continua = 'si'
     while (continua == 'si'):
         mostrarPlanta()
         numeroFila = int(input("Ingrese Fila"))
+        if not esValidaFila(numeroFila):
+            print("Numero de fila invalido")
+            continue
         columna = input("Ingrese Columna")
-        if estaVendidaButaca(numeroFila, columna):
+        if not esValidaColumna(columna):
+            print("Columna invalida")
+            continue
+        try:
+            venderButaca(numeroFila, columna)
+        except Exception as e:
             print("La butaca Fila=",numeroFila," Columna=",columna," fue vendida previamente.")
-        else:
-            grabarVenta(numeroFila, columna)
-            imprimirEntrada(numeroFila, columna)
         continua = input("continua? si/no")
     estadisticas()
               
